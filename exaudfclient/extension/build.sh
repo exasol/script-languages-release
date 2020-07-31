a06b69f4428ef150a95d06d95e087e3eeca8f784
Torsten Kilias <torsten.kilias@exasol.com>
2020-07-31 16:36:16 +0200

    update submodule and update exaudfclient build script to match the build script in script-languages submodule

--- exaudfclient/extension/build.sh
+++ exaudfclient/extension/build.sh
@@ -1,1 +1,5 @@
+if [ -z "$EXAUDF_BASEPATH" ]
+then
+      echo EXAUDF_BASEPATH=/exaudf/external
+fi
 bazel build $*
--- exaudfclient/extension/build_local.sh
+++ exaudfclient/extension/build_local.sh
@@ -1,2 +1,3 @@
 source .env
+export EXAUDF_BASEPATH="$PWD/bazel-bin/external"
 bash build.sh $*

2b9a7c466d67daed047ee25544480eabbb836147
tkilias <torsten.kilias@exasol.com>
2020-11-13 12:29:23 +0000

    Change flavor_base_deps of fancyr-EXASOL-6.1.0 to build libcurl with libssh support

--- flavors/fancyr-EXASOL-6.1.0/flavor_base/flavor_base_deps/compile_libcurl3_with_sftp.sh
+++ flavors/fancyr-EXASOL-6.1.0/flavor_base/flavor_base_deps/compile_libcurl3_with_sftp.sh
@@ -1,22 +1,32 @@
-sudo apt-get -y --no-install-recommends install wget build-essential debhelper libssh-dev
-sudo apt-get source curl
-sudo apt-get build-dep curl
+#!/bin/bash 
+
+set -e
+set -u
+set -o pipefail
+
+apt-get -y --no-install-recommends install wget build-essential debhelper libssh-dev
+apt-get source curl
+apt-get build-dep -y --no-install-recommends curl
 
 cd curl-*
 
-wget https://launchpadlibrarian.net/409184445/ubuntu_libssl.patch
-patch < ubuntu_libssl.patch
+cat debian/rules
 
-sudo dpkg-buildpackage -uc -us
+wget https://launchpadlibrarian.net/409184445/ubuntu_libssl.patch
+patch debian/rules < ubuntu_libssl.patch
+
+dpkg-buildpackage -uc -us -b
 # -us Do not sign the source package.
 # -uc Do not sign the .changes file.
 
 cd ..
 
-sudo dpkg -i curl_*.deb
-sudo dpkg -i libcurl3-*.deb
-sudo dpkg -i libcurl3-gnutls_*.deb
+dpkg -i curl_*.deb
+dpkg -i libcurl3-*.deb
+dpkg -i libcurl3-gnutls_*.deb
 
-sudo apt-mark hold curl
-sudo apt-mark hold libcurl3
-sudo apt-mark hold libcurl3-gnutls
+apt-mark hold curl
+apt-mark hold libcurl3
+apt-mark hold libcurl3-gnutls
+
+curl --v 2>&1 | grep sftp

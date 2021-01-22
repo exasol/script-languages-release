81c3262660ac9f2369a6e5ac7dabd6cce8eb0b69
Torsten Kilias <torsten.kilias@exasol.com>
2021-01-22 16:56:31 +0100

    Add first version which gets AWS credentials and fetches c4

---
+++ .github/workflows/create_aws_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILE_PREFIX="$1"
+
+RESPONSE=$($AWS_GET_ROLE)
+AWS_ACCESS_KEY_ID=$(echo "$RESPONSE" | jq -r .AWSAccessKeyID)
+AWS_SECRET_ACCESS_KEY=$(echo "$RESPONSE" | jq -r .AWSSecretAccessKey)
+AWS_SESSION_TOKEN=$(echo "$RESPONSE" | jq -r .AWSSessionToken)
+
+CREDENTIALS_FILE="$FILE_PREFIX/credentials"
+
+echo '[default-mfa]' > "$CREDENTIALS_FILE"
+echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> "$CREDENTIALS_FILE"
+echo "aws_session_token = $AWS_SESSION_TOKEN" >> "$CREDENTIALS_FILE"
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> "$CREDENTIALS_FILE"
+
+
+CONFIG_FILE="$FILE_PREFIX/config"
+
+echo '[profile default-mfa]' > "$CONFIG_FILE"
+echo 'region = eu-west-1' >>  "$CONFIG_FILE"
+

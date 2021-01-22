ed5a98789b98cce67268d3e15ac1ab85ad30df29
Torsten Kilias <torsten.kilias@exasol.com>
2021-01-22 18:57:31 +0100

    Remove debugging from create_ssh_key.sh

--- .github/workflows/create_ssh_key.sh
+++ .github/workflows/create_ssh_key.sh
@@ -4,15 +4,8 @@ set -euo pipefail
 
 set -x
 
-#(
+(
 
-FILE_PATH="$1"
+echo "$SSH_KEY_PEM" > $FILE_PATH
 
-ls -l $HOME/.ssh
-
-echo "FILE_PATH X${FILE_PATH}X"
-
-echo "TEST_PEM" > $FILE_PATH
-#echo "$SSH_KEY_PEM" > $FILE_PATH
-
-#) &> /dev/null
+) &> /dev/null

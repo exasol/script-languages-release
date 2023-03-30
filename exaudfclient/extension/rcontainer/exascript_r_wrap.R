44c8b85b9a91f6f796c74c2a1f60b99424e33c0d
Torsten Kilias <torsten.kilias@exasol.com>
2023-03-30 19:51:18 +0200

    Cleanup code

--- exaudfclient/extension/rcontainer/exascript_r_wrap.R
+++ exaudfclient/extension/rcontainer/exascript_r_wrap.R
@@ -123,12 +123,10 @@ INTERNAL_RUN_WRAPPER_BODY__ <- function(meta, inp, out) {
                 data <- run(context)
                 if (!is.null(data)) {
                   emitfun(data)
-                  if (!nextfun()) break
                 } else {
                   emitfun(NA)
-                  if (!nextfun()) break
-                  #stop(paste("F-UDF-CL-SL-R-: R null value not allowed as return value, please use NA to return a SQL NULL Value"))
 		};
+                if (!nextfun()) break
             }
             eo("F-UDF-CL-SL-R-1047",ResultHandler_flush)
         } else if (em("F-UDF-CL-SL-R-1048",Metadata_outputType) == "MULTIPLE") {

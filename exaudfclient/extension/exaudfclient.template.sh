a3c208fe3a56ba7a6c89b62eba7fd9399a7c00fc
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-08-14 17:53:34 -0300

    #685: updated bazel (#942)
    fixes #685 
    
    - Use new MODULE.bazel instead of WORKSPACE
      - Convert the base part in Script-Languages to a submodule, which then can be used from the exaudfclient in Script-Languages and Script-Languages-Release. This simplifies the packaging of the UDF client in the Script-Languages-Container as we don't need to mess around with symlinks for the binaries in the former Script-Languages Bazel package.
     - Convert the rules for external packages (zmq, protobuf, Python, Java,...) to be complient to the new MODULE implementation
     - Use MODULE dependencies were applicable
        - Maven
    - Refactor the Java BUILD file
      - The new version of Bazel does not support directories in the out attribute of a rule. We need to specify each single output file
      - Removed unnecessary rules
      - Pass only needed files to the filter rules
     - Clean up Conda flavors
       - Pin only given packages, but not all (already installed) packages
       - Remove transitive dependencies in conda package files
       - Test if CUDA flavors still work correctly with Pytorch and Tensorflow + Nvidia GPU
       -  Remove dependency to OpenJDK: Bazel defines this dependency. We don't need it explicitely.
    
    ---------
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>

--- exaudfclient/extension/exaudfclient.template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-echo "Changing to script directory  $SCRIPT_DIR"
-cd $SCRIPT_DIR
-export LIBEXAUDFLIB_PATH="$SCRIPT_DIR/external/script_languages/libexaudflib_complete.so"
-export LIBPYEXADATAFRAME_DIR="$SCRIPT_DIR/external/script_languages/python/python3"
--- exaudfclient/rcontainer/rcontainer.cc
+++ exaudfclient/rcontainer/rcontainer.cc
@@ -8,7 +8,7 @@
 #include <R_ext/Boolean.h>
 #include "exascript_r.h"
 #include "exascript_r_int.h"
-#include "exaudflib/swig/swig_result_handler.h"
+#include "base/exaudflib/swig/swig_result_handler.h"
 
 using namespace SWIGVMContainers;
 using namespace std;
